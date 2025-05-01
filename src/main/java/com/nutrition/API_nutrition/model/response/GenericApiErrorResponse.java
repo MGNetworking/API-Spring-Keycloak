c6b3c1cd9a5a556bb80fea9b9762d17cff9f854d
Maxime <ghalem.maxime@gmail.com>
2025-05-01 23:37:46 +0200

    add: classe GenericApiErrorResponse

---
+++ src/main/java/com/nutrition/API_nutrition/model/response/GenericApiErrorResponse.java
@@ -0,0 +1,32 @@
+package com.nutrition.API_nutrition.model.response;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@JsonPropertyOrder({
+        "timestamp",
+        "status",
+        "statusCode",
+        "message",
+        "path",
+        "errorCode",
+        "data"
+})
+@Data
+@NoArgsConstructor
+public class GenericApiErrorResponse<D> extends GenericApiResponse<D> {
+
+    private String errorCode;
+
+    public GenericApiErrorResponse(HttpStatus status,
+                                   int statusCode,
+                                   String message,
+                                   String path,
+                                   D data,
+                                   String errorCode) {
+        super(status, statusCode, message, path, data);
+        this.errorCode = errorCode;
+    }
+}

4b8f1e5e3c97fb976deb71d896617fab2ab44e02
Maxime <ghalem.maxime@gmail.com>
2025-05-01 23:34:22 +0200

    add: class Generic Response - GenericApiErrorResponse - GenericApiResponse

--- src/main/java/com/nutrition/API_nutrition/model/response/GenericApiResponse.java
+++ src/main/java/com/nutrition/API_nutrition/model/response/GenericApiResponse.java
@@ -1,24 +1,34 @@
 package com.nutrition.API_nutrition.model.response;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
-import java.time.LocalDate;
-
+import java.time.LocalDateTime;
+@JsonPropertyOrder({
+        "timestamp",
+        "status",
+        "statusCode",
+        "message",
+        "path",
+        "data"
+})
 @Data
 @NoArgsConstructor
 public class GenericApiResponse<D> {
 
-    private String timestamp;
+    private LocalDateTime timestamp;
     private HttpStatus status;
+    private int statusCode;
     private String message;
     private String path;
     private D data;
 
-    public GenericApiResponse(HttpStatus status, String message, String path, D data) {
-        this.timestamp = LocalDate.now().toString();
+    public GenericApiResponse(HttpStatus status, int statusCode, String message, String path, D data) {
+        this.timestamp = LocalDateTime.now();
         this.status = status;
+        this.statusCode = statusCode;
         this.message = message;
         this.path = path;
         this.data = data;

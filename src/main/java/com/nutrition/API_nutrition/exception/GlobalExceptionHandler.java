4e8ea093f5c0052d9d8ede6b4649822447b13aa8
Maxime <ghalem.maxime@gmail.com>
2025-05-13 16:37:56 +0200

    add in GlobalExceptionHandler this ExceptionHandler customise: - MissingRequestHeaderException - HttpMessageNotReadableException - BindException
    And add customise MethodArgumentNotValidException

--- src/main/java/com/nutrition/API_nutrition/exception/GlobalExceptionHandler.java
+++ src/main/java/com/nutrition/API_nutrition/exception/GlobalExceptionHandler.java
@@ -6,20 +6,25 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.net.BindException;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @ControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+public class GlobalExceptionHandler {
 
     /**
      * Gestionnaire d'exception personnalisé pour les exceptions metier
-     *
-     * @param apiException
-     * @param request
-     * @return
      */
     @ExceptionHandler(ApiException.class)
     public ResponseEntity<GenericApiErrorResponse<ApiResponseData>> handleApiException(
@@ -33,7 +38,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 apiException.getHttpStatus().value(),
                 apiException.getMessage(),
                 request.getRequestURI(),
-                null, // data
+                null,
                 apiException.getErrorCode()
         );
 
@@ -43,8 +48,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     /**
-     * @param ex
-     * @return
+     * Gestion des exceptions génériques
      */
     @ExceptionHandler(Exception.class)
     public ResponseEntity<GenericApiErrorResponse<ApiResponseData>> handleGenericException(
@@ -66,4 +70,99 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(response);
     }
+
+    /**
+     * Gestionnaire pour les exceptions HTTP communes
+     */
+    @ExceptionHandler({
+            MissingRequestHeaderException.class,
+            HttpMessageNotReadableException.class,
+            BindException.class,
+    })
+    public ResponseEntity<GenericApiErrorResponse<Map<String, Object>>> handleHttpRequestExceptions(Exception ex, HttpServletRequest request) {
+
+        log.error("Unhandled http generic exception: {}", ex.getMessage(), ex);
+
+        Map<String, Object> data = new LinkedHashMap<>();
+
+        // Message par défaut pour toutes ces exceptions
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        String errorCode = "BAD_REQUEST";
+        String message = "Bad request";
+
+        // Message Personnalisé en fonction du type d'exception
+        if (ex instanceof MissingRequestHeaderException) {
+            data.put("message", "En-tête manquant: " + ((MissingRequestHeaderException) ex).getHeaderName());
+            errorCode = "MISSING_HEADER";
+            message = "Missing required header";
+
+        } else if (ex instanceof HttpMessageNotReadableException) {
+            data.put("message", "Corps de requête invalide ou manquant");
+            errorCode = "INVALID_REQUEST_BODY";
+            message = "Invalid request body";
+
+        } else if (ex instanceof BindException) {
+            data.put("message", "Binding error");
+            errorCode = "BINDING_ERROR";
+            message = "Request binding error";
+
+        } else {
+            data.put("message", ex.getMessage());
+        }
+
+        GenericApiErrorResponse<Map<String, Object>> response = new GenericApiErrorResponse<>(
+                status,
+                status.value(),
+                "An unexpected http error has occurred",
+                request.getRequestURI(),
+                data,
+                "UNEXPECTED_HTTP_ERROR"
+        );
+
+        return ResponseEntity.status(status).body(response);
+    }
+
+    /**
+     * Gestionnaire pour les exceptions
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<GenericApiErrorResponse<Map<String, Object>>> handleValidationExceptions(
+            MethodArgumentNotValidException ex,
+            HttpServletRequest request) {
+
+        BindingResult result = ex.getBindingResult();
+
+        // Cas typique : erreurs sur des champs d’un DTO
+        if (result.hasFieldErrors()) {
+            List<String> validationErrors = result.getFieldErrors()
+                    .stream()
+                    .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                    .collect(Collectors.toList());
+
+            Map<String, Object> data = new LinkedHashMap<>();
+            data.put("errors", validationErrors);
+
+            return ResponseEntity.badRequest().body(new GenericApiErrorResponse<>(
+                    HttpStatus.BAD_REQUEST,
+                    HttpStatus.BAD_REQUEST.value(),
+                    "DTO validation failed",
+                    request.getRequestURI(),
+                    data,
+                    "VALIDATION_ERROR"
+            ));
+        }
+
+        // Sinon, on est peut-être dans un autre cas d'erreur (ex : internal error de Spring)
+        Map<String, Object> data = Map.of("message", "Unexpected validation failure");
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new GenericApiErrorResponse<>(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Unexpected validation context",
+                request.getRequestURI(),
+                data,
+                "UNEXPECTED_VALIDATION_CONTEXT"
+        ));
+    }
+
 }
